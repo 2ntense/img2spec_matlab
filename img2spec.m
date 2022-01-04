603908d8433309257c366f80941ca74bcaf25285
2ntense <stefanyip@hotmail.com>
2022-01-04 16:14:21 +0100

    Changed dynamically sized row vector to static

--- img2spec.m
+++ img2spec.m
@@ -1,18 +1,17 @@
 function img2spec(img_path, out_path, duration, img_freq_range, img_time_range, samp_rate, freq_step, samp_step)
     img_duration = img_time_range(2) - img_time_range(1);
     img_data = load_img(img_path, freq_step, samp_rate, img_duration);
-
     img_data = flipud(img_data);
     img_data = transpose(img_data);
-
-    out_signal = [];
-
+    out_signal = zeros(1, duration * samp_rate);
     img_size = size(img_data);
 
     for x = 1:samp_step:img_size(1)
+        idx_start = x + (img_time_range(1) * samp_rate);
+        idx_end = idx_start + samp_step - 1;
         signal = zeros(1, samp_step);
         if min(img_data(x,:)) == 255
-            out_signal = [out_signal signal];
+            out_signal(idx_start:idx_end) = signal;
             continue;
         end
         for y = img_size(2):-1:1
@@ -22,20 +21,15 @@ function img2spec(img_path, out_path, duration, img_freq_range, img_time_range, 
                 signal = signal + gen_wave(rescaled_freq, amp, samp_step, samp_rate);
             end
         end
-        out_signal = [out_signal signal];
+        out_signal(idx_start:idx_end) = signal;
     end
     rescaled_out_signal = rescale(out_signal, -1, 1);
-    left_padding = img_time_range(1) * samp_rate;
-    right_padding = (duration - img_time_range(2)) * samp_rate;
-    % audiowrite(out_path, rescaled_out_signal, samp_rate);
-    audiowrite(out_path, [zeros(1, left_padding) rescaled_out_signal zeros(1, right_padding)], samp_rate);
-    
-
+    audiowrite(out_path, rescaled_out_signal, samp_rate);
 end
 
 function img_data = load_img(img_path, freq_step, samp_rate, duration)
     img_data = imread(img_path);
-    img_data = rgb2gray(img_data);
+    img_data = im2gray(img_data);
     
     new_rows = (samp_rate / 2) / freq_step;
     new_cols = duration * samp_rate;
