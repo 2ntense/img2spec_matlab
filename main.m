fac7361e3df528375ffe885ec0ab5c575b2c4bbd
2ntense <stefanyip@hotmail.com>
2022-01-13 17:38:32 +0100

    Add example program

---
+++ main.m
@@ -0,0 +1,32 @@
+img_file = "txt.png";
+audio_file = "neoncity.wav";
+out_file = "out.wav";
+samp_rate = 44100;
+duration = 441000 / samp_rate;
+start_freq = 18000;
+end_freq = 20000;
+start_time = 0;
+end_time = 10;
+freq_step = 175;
+samp_step = 1050;
+
+lorem_ispum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+imwrite(text2im(lorem_ispum), img_file);
+img_signal = img2spec(img_file, duration, [start_freq end_freq], [start_time end_time], samp_rate, freq_step, samp_step);
+audio_signal = audioread(audio_file);
+
+mixed_signal = mix_signals(audio_signal, img_signal, 0, 0);
+base_snr = snr(audio_signal, mixed_signal - audio_signal);
+db_diff = 55 - base_snr;
+mixed_signal = mix_signals(audio_signal, img_signal, 0, -db_diff);
+
+% show_spec(mixed_signal, 1024, 1024*0.75, 0:(samp_rate/2), samp_rate);
+
+signal_to_noise = snr(audio_signal, mixed_signal - audio_signal);
+
+audiowrite(out_file, mixed_signal, samp_rate);
+
+function show_spec(signal, win_size, win_overlap, freq_range, samp_rate)
+    spectrogram(signal, win_size, win_overlap, freq_range, samp_rate, "yaxis");
+end
